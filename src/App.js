5255b89706dcefd850e0e299d879fa8d62ba65b7
Dhrumil Contractor <dhrumil@topsinfosolutions.com>
2023-08-07 19:51:00 +0530

    Added Basic Speech Detective Functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,72 @@
+import { useRef, useState, useEffect } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import "./App.css";
 
 function App() {
-  return <div className="App">Voice Detectic App</div>;
+  const { browserSupportsSpeechRecognition } = useSpeechRecognition();
+
+  const [isListening, setIsListening] = useState(false);
+
+  const microphoneRef = useRef(null);
+
+  useEffect(() => {}, []);
+
+  if (!browserSupportsSpeechRecognition) {
+    return (
+      <div className="mircophone-container">
+        Browser is not Support Speech Recognition.
+      </div>
+    );
+  }
+  const handleListing = () => {
+    setIsListening(true);
+    microphoneRef.current.classList.add("listening");
+    SpeechRecognition.startListening({
+      continuous: true,
+    });
+  };
+  const stopHandle = () => {
+    setIsListening(false);
+    microphoneRef.current.classList.remove("listening");
+    SpeechRecognition.stopListening();
+  };
+
+  return (
+    <div className="microphone-wrapper">
+      <div className="mircophone-container">
+        <div
+          className="microphone-icon-container"
+          ref={microphoneRef}
+          onClick={handleListing}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z"
+            />
+          </svg>
+        </div>
+        {isListening && console.log("speaking")}
+        <div className="microphone-status">
+          {isListening ? "Speaking........." : "Click to start Speaking"}
+        </div>
+        {isListening && (
+          <button className="microphone-stop btn" onClick={stopHandle}>
+            Stop
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
